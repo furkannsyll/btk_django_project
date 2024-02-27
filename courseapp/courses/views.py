b601df796551b96f00abe60be5a4924055ad342e
Furkan Soylu <fsoylu567@gmail.com>
2024-02-27 22:09:39 +0300

    Url Redirects

--- courseapp/courses/views.py
+++ courseapp/courses/views.py
@@ -1,5 +1,11 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound
+from django.shortcuts import redirect, render
+
+data = {
+    "programming":"Courses in the Programming Category",
+    "web-development":"Courses in the Web Development Category",
+    "mobile":"Courses in the Mobile Category"
+}
 
 def courses(request):
     return HttpResponse('Course List')
@@ -8,15 +14,16 @@ def details(request, course_name):
     return HttpResponse(f"{course_name} detail page")
 
 def getCoursesByCategoryName(request, category_name):
-    text = ''
-    if(category_name == "programming"):
-        text = "List of courses in the Programming Category"
-    elif(category_name == "web-development"):
-        text = "List of courses in the Web Development Category"
-    else:
-        text = "False Category Selection"
-        
-    return HttpResponse(text)
+    try:
+        category_text = data[category_name]
+        return HttpResponse(category_text)
+    except:
+        return HttpResponseNotFound('False Category Selection')
 
 def getCoursesByCategoryId(request, category_id):
-    return HttpResponse(category_id)
+    category_list = list(data.keys())
+    if(category_id > len(category_list)):
+        return HttpResponseNotFound('False Category Selection')
+    redirect_text = category_list[category_id - 1]
+    
+    return redirect('/course/category/' + redirect_text)
