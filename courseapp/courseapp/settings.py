8ce71d627e85adf8eb86507996fc88537b142f90
Furkan Soylu <fsoylu567@gmail.com>
2024-02-29 22:32:43 +0300

    Layouts

--- courseapp/courseapp/settings.py
+++ courseapp/courseapp/settings.py
@@ -45,7 +45,10 @@ ROOT_URLCONF = 'courseapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [
+            BASE_DIR / 'templates', 
+            os.path.join(BASE_DIR, 'templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
