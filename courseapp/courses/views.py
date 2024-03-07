c8c514ff5c859e1cc7c33d1047d969729a7913ba
Furkan Soylu <fsoylu567@gmail.com>
2024-03-07 15:11:06 +0300

    Implementation of many-to-many relationships in the admin panel

--- courseapp/courses/views.py
+++ courseapp/courses/views.py
@@ -20,7 +20,7 @@ def details(request, slug):
     return render(request, 'courses/details.html', context)
 
 def getCoursesByCategoryName(request, slug):
-    courses = Course.objects.filter(category__slug=slug, isActive=True)
+    courses = Course.objects.filter(categories__slug=slug, isActive=True)
     categories = Category.objects.all()
 
     return render(request, 'courses/index.html', {'categories': categories, 'courses': courses, 'selectedCategory': slug})
