1f4741433ea7f51d8558155af6a2594030bfad44
Furkan Soylu <fsoylu567@gmail.com>
2024-03-11 16:24:07 +0300

    _Form Properties

--- courseapp/courses/forms.py
+++ courseapp/courses/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
 class CourseCreateForm(forms.Form):
-    title = forms.CharField()
+    title = forms.CharField(label="Course Title", required=True, error_messages={"required": "You must enter the Course Title"})
     description = forms.CharField(widget=forms.Textarea)
     imageUrl = forms.CharField()
     slug = forms.SlugField()
