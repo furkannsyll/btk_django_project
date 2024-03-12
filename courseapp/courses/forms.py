b04ae82f5253ad615b7a606c89fb58f6186af952
Furkan Soylu <fsoylu567@gmail.com>
2024-03-12 15:49:59 +0300

    _Edit Form

--- courseapp/courses/forms.py
+++ courseapp/courses/forms.py
@@ -37,3 +37,29 @@ class CourseCreateForm(forms.ModelForm):
                 "required": "You must enter the Course description",
             }
         }
+
+class CourseEditForm(forms.ModelForm):
+    class Meta:
+        model = Course
+        # fields = "__all__"
+        fields = ('title','description','imageUrl','slug','categories','isActive')
+        labels = {
+            'title': 'Course Title',
+            'description': 'Course Description'
+        },
+        widgets = {
+            'title': forms.TextInput(attrs={"class":"form-control"}),
+            'description': forms.Textarea(attrs={"class":"form-control"}),
+            'imageUrl': forms.TextInput(attrs={"class":"form-control"}),
+            'slug': forms.TextInput(attrs={"class":"form-control"}),
+            'categories': forms.SelectMultiple(attrs={"class":"form-control"})
+        }
+        error_messages = {
+            'title': {
+                "required": "You must enter the Course Title",
+                "max_length": "You must enter a maximum of 50 characters"
+            },
+            'description': {
+                "required": "You must enter the Course description",
+            }
+        }
