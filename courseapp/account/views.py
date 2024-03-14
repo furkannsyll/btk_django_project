3d83e1b3529db439413d0b97d18fb446f17c321a
Furkan Soylu <fsoylu567@gmail.com>
2024-03-14 17:34:17 +0300

    _Messages

--- courseapp/account/views.py
+++ courseapp/account/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
+from django.contrib import messages
 
 # Create your views here.
 
@@ -16,13 +17,15 @@ def user_login(request):
 
         if user is not None:
             login(request, user)
+            messages.add_message(request, messages.SUCCESS, "Login successful")
             nextUrl = request.GET.get("next", None)
             if nextUrl is None:
                 return redirect("index")
             else:
                 return redirect(nextUrl)
         else:
-            return render(request, "account/login.html", { "error":"Username or Password is incorrect"})
+            messages.add_message(request, messages.ERROR, "Username or Password is incorrect")
+            return render(request, "account/login.html")
     else:
         return render(request, "account/login.html")
 
@@ -49,5 +52,6 @@ def user_register(request):
         return render(request, "account/register.html")
 
 def user_logout(request):
+    messages.add_message(request, messages.SUCCESS, "Logout successful")
     logout(request)
     return redirect("index")
--- courseapp/courseapp/settings.py
+++ courseapp/courseapp/settings.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+from django.contrib.messages import constants as messages
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -124,3 +125,11 @@ MEDIA_URL = "/media/"
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL = "/account/login"
+
+MESSAGE_TAGS = {
+    messages.DEBUG: "alert-secondary",
+    messages.INFO: "alert-info",
+    messages.SUCCESS: "alert-success",
+    messages.WARNING: "alert-warning",
+    messages.ERROR: "alert-danger",
+}
