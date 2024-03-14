f96b03c63e1f72e9ec7ae62d9f2afd5d9d938566
Furkan Soylu <fsoylu567@gmail.com>
2024-03-14 13:43:17 +0300

    _User Register

--- courseapp/account/views.py
+++ courseapp/account/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 
 # Create your views here.
 
@@ -22,7 +23,26 @@ def user_login(request):
         return render(request, "account/login.html")
 
 def user_register(request):
-    return render(request, "account/register.html")
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+        password = request.POST["password"]
+        re_password = request.POST["re_password"]
+
+        if password == re_password:
+            if User.objects.filter(username = username).exists():
+                return render(request, "account/register.html", { "error":"Username is used, Please enter another username value", "username": username, "email": email})
+            else:
+              if User.objects.filter(email = email).exists():
+                  return render(request, "account/register.html", { "error":"Email is used, Please enter another email value", "username": username, "email": email})
+              else:
+                  user = User.objects.create_user(username = username, email = email, password = password)
+                  user.save()
+                  return redirect("user_login")
+        else:
+            return render(request, "account/register.html", { "error":"Passwords do not match", "username": username, "email": email})
+    else:
+        return render(request, "account/register.html")
 
 def user_logout(request):
     logout(request)
