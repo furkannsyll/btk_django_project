c4da3d79328a3679c3743676fed8e82afdada2db
Furkan Soylu <fsoylu567@gmail.com>
2024-03-19 14:00:03 +0300

    _Django Rest Framework

--- api/books/views.py
+++ api/books/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from books.models import Book
 
-def list(request):
-    pass
+def book_list(request):
+   books = Book.objects.all()
+   books_list = list(books.values())
+   return JsonResponse({
+    #    "books": ["book 1, book 2"]
+    #    "books": [
+    #        {"id":1,"title":"book 1"},
+    #        {"id":2,"title":"book 2"},
+    #    ]
+        "books": books_list
+   })
 
 def create(request):
     pass
